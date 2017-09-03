d0d60aa590d54c9d9ec6e55de124095cc65c1bf5
Mehmet Dogan <info@intelligentworks.ch>
2017-09-03 09:18:50 +0200

    first commit

---
+++ js/tests.js
@@ -0,0 +1,14 @@
+/**
+ * Please use the file "js/chapters/chapter-xx.js" for your functions.
+ *
+ */
+
+
+describe('Chapter 01 - Simple Functions', () => {
+
+    it('01) coming soon', () => {
+        let result = myfunc();
+        
+        result.should.be.deepEqual(["Swiss"]);
+    });
+});
