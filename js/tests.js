ee1a0c46bd5fcdd5ca631be446a1b0291496bcae
Mehmet Dogan <info@intelligentworks.ch>
2017-09-04 15:05:21 +0200

    add new questions

--- js/tests.js
+++ js/tests.js
@@ -104,5 +104,25 @@ describe('Test Suite - Simple Functions', () => {
     it('20) Write a function that sums all even numbers to the given upper limit.', () => {
         let result = sumEventNumbersTo(193);
         result.should.be.equal(9312);
+    });    
+    
+    it('21) Write a function to convert a string into camel case.', () => {
+        let result = toCamelCase("it club volketwil");
+        result.should.be.equal("itClubVolketswil");
+    });    
+ 
+    it('22) Implement a function that finds words in a string which includes any of the given chars.', () => {
+        let result = findWordsByChars("Every saturday, we have a it-club checkpoint!", ["o", "u"]);
+        result.should.be.deepEqual(["saturday", "it-club", "checkpoint"]);
+    });
+    
+    it('23) Implement a function which produces random numbers between 1 and 10', () => {
+        let result = generateRandomNumbers();
+        result.should.be.withIn(1, 10);
+    });
+    
+    it('24) Write a function which create a complex password with 8 chars including at least one letter and one numner', () => {
+        let result = suggestPassword();
+        result.length.should.matche(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
     });     
 });
