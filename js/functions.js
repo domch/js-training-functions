f5f3bada11ed42564e7c678070bfc0d66e550660
Mehmet Dogan <info@intelligentworks.ch>
2017-09-05 20:05:57 +0200

    solve horoscope problem

--- js/functions.js
+++ js/functions.js
@@ -42,6 +42,38 @@ function getMonthName(pDate){
 
 function getHoroscope(pDate){
     
+    var month = pDate.getMonth() + 1;
+    var day = pDate.getDate();
+    
+    var horoscope = null;
+    
+    if((month == 1 && day <= 20) || (month == 12 && day >=22)) {
+        horoscope = "Oğlak";
+    } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+        horoscope = "Kova";
+    } else if((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        horoscope = "Balık";
+    } else if((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+        horoscope = "Koç";
+    } else if((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+        horoscope = "Boğa";
+    } else if((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        horoscope = "İkizler";
+    } else if((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+        horoscope = "Yengeç";
+    } else if((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+        horoscope = "Aslan";
+    } else if((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+        horoscope = "Başak";
+    } else if((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+        horoscope = "Terazi";
+    } else if((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+        horoscope = "Akrep";
+    } else if((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+        horoscope = "Yay";
+    }
+    
+    return horoscope;
 }
     
 function findFactorial(pLimit){
@@ -50,8 +82,13 @@ function findFactorial(pLimit){
     }, 1);
 }
     
-// Alternative way?
+
 function capitalize(pWord){
+    return pWord.charAt(0).toUpperCase() + 
+           pWord.slice(1, pWord.length);
+}
+// Alternative way?
+function capitalizeAlternative(pWord){
     return pWord.split("")
                 .map(function(pValue, pIndex, pArray){
                     if(pIndex == 0)
@@ -61,15 +98,45 @@ function capitalize(pWord){
                 .join("");
 }
     
-function findSocialLevel(150000){}
+function findSocialLevel(150000){
+    if(pSalary >= 150000)
+        return "GOOD";
+    else if(pSalary >= 100000)
+        return "NOT BAD";
+    else if(pSalary >= 50000)
+        return "NOT ENOUGH";
+    else 
+        return "POOR";
+}
     
-function findEvenNumbersTo(9){}
+function findEvenNumbersTo(pLimit){
+    return new Array(pLimit)
+            .map(function(pValue, pIndex){
+               return pIndex; 
+            })
+            .filter(function(pValue){
+                return pValue % 2 == 0;
+            });
+}
     
-function findTheSequenceTo(13, 4){}
+function findTheSequenceTo(pLimit, pStep){
+    return new Array(pLimit)
+            .map(function(pValue, pIndex){
+               return pIndex; 
+            })
+            .filter(function(pValue){
+                return pValue % pStep == 0;
+            })
+
+}
     
-function concatSpecially("it", "club"){}  
+function concatSpecially(pFirstWord, pSecondWord){
+    return pFirstWord.slice(1, pFirstWord.length).concat(pSecondWord.slice(1, pSecondWord.length));
+}  
    
-function orderAlphabetically("itclub"){}
+function orderAlphabetically("itclub"){
+    
+}
     
 function findTheLongestWord("Herzlich willkommen zu unseren itclub Übungen. Hier sehen Sie Ihre Verbesserungspotenziel"){}
     
